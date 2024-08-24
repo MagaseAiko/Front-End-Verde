5e386d6f08481e3bc135171ac5230bf53d3b2082
MagaseAiko <aiko40@outlook.com>
2024-08-24 09:43:35 -0300

    teste

--- src/FrontEndVerde.jsx
+++ src/FrontEndVerde.jsx
@@ -30,8 +30,8 @@ function FrontEndVerde(){
         setMostrarCorpo(true);
     };
 
-    const lista = ["./src/assets/Rec_Plastico.png", "./src/assets/Rec_Tereftalato.png", "./src/assets/Rec_Alta_Densidade.png", "./src/assets/Rec_Vinila.png",
-                    "./src/assets/Rec_Baixa_Densidade.png", "./src/assets/Rec_Polipropileno.png", "./src/assets/Rec_Poliestireno.png", "./src/assets/Rec_Outros.png"
+    const lista = ["./Front-End-Verde/src/assets/Rec_Plastico.png", "./Front-End-Verde/src/assets/Rec_Tereftalato.png", "./Front-End-Verde/src/assets/Rec_Alta_Densidade.png", "./Front-End-Verde/src/assets/Rec_Vinila.png",
+                    "./Front-End-Verde/src/assets/Rec_Baixa_Densidade.png", "./Front-End-Verde/src/assets/Rec_Polipropileno.png", "./Front-End-Verde/src/assets/Rec_Poliestireno.png", "./Front-End-Verde/src/assets/Rec_Outros.png"
     ]
 
     const nomes_reciclagem = ["♻ Reciclagem de Plástico:", "♻1 (PET) Polietileno tereftalato:", "♻2 (PEAD) Polietileno de Alta Densidade:", "♻3 (PVC) Cloreto de Polivinil:",
@@ -55,16 +55,16 @@ function FrontEndVerde(){
         <div>
             <header>
                 <div className= "divnome">
-                    <img style={{width: '50px'}} src="./src/assets/icon3.png"/>
+                    <img style={{width: '50px'}} src="./Front-End-Verde/src/assets/icon3.png"/>
                     <div className="nome">Nome do Site</div>
                 </div>
 
                 <div>
-                    <img className ="filter-white" style={{width: "50px"}} src="./src/assets/icon4.svg" onClick={mostrarSidebar}/>
+                    <img className ="filter-white" style={{width: "50px"}} src="./Front-End-Verde/src/assets/icon4.svg" onClick={mostrarSidebar}/>
                 </div>
 
                 <div className={`barralateral ${barraLateral ? 'open' : ''}`}>
-                    <img className ="filter-white" style={{width: "45px"}} src="./src/assets/close.svg" onClick={mostrarSidebar}/>
+                    <img className ="filter-white" style={{width: "45px"}} src="./Front-End-Verde/src/assets/close.svg" onClick={mostrarSidebar}/>
                     <ul>
                     <li>Home</li>
                     <li>Buscador de Decarte</li>
@@ -91,7 +91,7 @@ function FrontEndVerde(){
             {mostrarCorpo ? (
             <div className="corpo">
                 <div className="titulo">
-                    <img id="rodar_reciclagem" style={{width: "50px"}} src="./src/assets/icon2.png"/>
+                    <img id="rodar_reciclagem" style={{width: "50px"}} src="./Front-End-Verde/src/assets/icon2.png"/>
                     <h1>APRENDA A RECICLAR</h1>
                 </div>
                 
@@ -101,13 +101,13 @@ function FrontEndVerde(){
 
                 <div className="png">
                     <div className="box_imagens">
-                        <img className="imagens" src="./src/assets/Salgadinho.png"></img>   
+                        <img className="imagens" src="./Front-End-Verde/src/assets/Salgadinho.png"></img>   
                     </div>
                     <div className="box_imagens">
-                        <img className="imagens" src="./src/assets/Garrafa.png" ></img>  
+                        <img className="imagens" src="./Front-End-Verde/src/assets/Garrafa.png" ></img>  
                     </div>
                 </div>
-                <img id="scroll" className="filter-white" style={{width: "50px"}} href='#meuelemento' src="./src/assets/arrow.svg" onClick={()=>{window.scroll({top: window.innerHeight})}}/>
+                <img id="scroll" className="filter-white" style={{width: "50px"}} href='#meuelemento' src="./Front-End-Verde/src/assets/arrow.svg" onClick={()=>{window.scroll({top: window.innerHeight})}}/>
 
                 <div className="tipos_embalagem"> 
                     <h2>Clique no ícone que você encontrou em sua embalagem para saber mais.</h2>
@@ -122,10 +122,10 @@ function FrontEndVerde(){
                 <div className={"tiposReciclagem open"}>
                     <div className="cabecalho">
                         <div className="voltar" onClick={voltarAoCorpo}>Voltar
-                            <img className ="filter-white" style={{width: "45px"}} src="./src/assets/left_arrow.svg" onClick={voltarAoCorpo}/>
+                            <img className ="filter-white" style={{width: "45px"}} src="./Front-End-Verde/src/assets/left_arrow.svg" onClick={voltarAoCorpo}/>
                         </div>
                         <div className="titulo">
-                            <img id="rodar_reciclagem" style={{width: "50px"}} src="./src/assets/icon2.png"/>
+                            <img id="rodar_reciclagem" style={{width: "50px"}} src="./Front-End-Verde/src/assets/icon2.png"/>
                             <h1>APRENDA A RECICLAR</h1>
                         </div> 
                     </div>
@@ -134,7 +134,7 @@ function FrontEndVerde(){
                         <div className="explicação" dangerouslySetInnerHTML={{ __html: descricoes[itemSelecionado] }}></div>
                         <div className="box_ilustrativa">
                             <div className="tipo_lixo">
-                                <img style={{width: "200px"}} src="./src/assets/Reciclagem_Plastico.png"/>
+                                <img style={{width: "200px"}} src="./Front-End-Verde/src/assets/Reciclagem_Plastico.png"/>
                             </div>
                             <div className="descrição">
                                 <div className="local_descarte_img">
